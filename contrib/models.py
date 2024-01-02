f8adbc7b595bfdaa8dc6f5719387e6c8d0887c71
Marcelo Alves <130853760+marcelodev23@users.noreply.github.com>
2024-01-01 22:57:49 -0300

    Delete contrib directory

--- contrib/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapper_column
-from sqlalchemy import UUID
-from uuid import uuid4
-from sqlalchemy.dialects.postgresql import UUID as pg_UUID
-
-class BaseModel(DeclarativeBase):
-    id: Mapped[UUID] = mapper_column(pg_UUID(as_uuid=True),defaul=uuid4,nullable=False)
